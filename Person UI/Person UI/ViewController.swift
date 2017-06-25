de84b342c0c08d72e387fd5110465bde1a7a5f59
Taylor Snyder <tsny@Taylors-MacBook-Pro.local>
2017-06-24 23:58:33 -0500

    init commit

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Taylor Snyder on 6/24/17.
+//  Copyright © 2017 Taylor Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
